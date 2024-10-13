401b61dfd070c5c6e74e44131982ba7f09f218e5
sammy-93730 <sammynghipandwa263@gmail.com>
2024-10-13 23:53:57 +0200

    Add files via upload

---
+++ displayArea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author isaia
+ */
+class displayArea {
+
+    static void setText(String no_contacts_available) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
