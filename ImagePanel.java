7ca62e7b190010332c63199b60cbe2d76dc7a11c
sammy-93730 <sammynghipandwa263@gmail.com>
2024-10-14 21:37:11 +0200

    Add files via upload

---
+++ ImagePanel.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ImagePanel extends JPanel {
+    private Image backgroundImage;
+
+    public ImagePanel(String fileName) {
+        backgroundImage = new ImageIcon(fileName).getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+    }
+}
